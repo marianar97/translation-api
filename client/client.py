9afac0bf636e521c9be234c76d1a8aada4048070
Mariana <marianar@andrew.cmu.edu>
2024-11-08 14:32:12 -0500

    fixing incosistency in client.py

--- client/client.py
+++ client/client.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI, BackgroundTasks
 from client.models import TranslationRequest, JobResponse
 from client.services.translation_service import TranslationService
 
+app = FastAPI(
+    title="Translation API",
+    description="API for handling translation jobs",
+    version="1.0.0"
+)
 
-app = FastAPI()
-
-@app.post("/translations/job/", status_code=201)
+@app.post("/translations/job", status_code=201)
 async def create_translation_job(
-    request: TranslationRequest, background_tasks: BackgroundTasks
+    request: TranslationRequest, 
+    background_tasks: BackgroundTasks
 ) -> JobResponse:
     job = await TranslationService.create_translation(request)
     logger.info(f"Created translation job with ID: {job.id}")
@@ -16,8 +20,7 @@ async def create_translation_job(
     logger.info(f"Background task added to monitor job status for job ID: {job.id}")
     return job
 
-
-@app.get("/translation/job/{id}", status_code=200)
+@app.get("/translations/job/{id}", status_code=200)
 async def get_job(id: str):
     logger.info(f"Received request to get job with ID: {id}")
     job = await TranslationService.get_job(id)
@@ -27,7 +30,7 @@ async def get_job(id: str):
         logger.warning(f"Job with ID: {id} not found")
     return job
 
-if __name__ == '__main__':
-    logger.info("Starting FastAPI Client Library")
-    import uvicorn
-    uvicorn.run(app)
+# if __name__ == '__main__':
+#     logger.info("Starting FastAPI Client Library")
+#     import uvicorn
+#     uvicorn.run(app)
