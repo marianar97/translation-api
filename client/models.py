88038fed044994c428560ca3f9a51d832b75765d
Mariana <marianar@andrew.cmu.edu>
2024-11-08 15:46:46 -0500

    adding validation to client.models

--- client/models.py
+++ client/models.py
@@ -1,6 +1,6 @@
 from enum import Enum
 import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class Status(str, Enum):
@@ -23,12 +23,24 @@ class TranslationRequest(BaseModel):
         description="url endpoint to notify once the translation is completed"
     )
 
+    @field_validator("webhook_url")
+    def validate_webhook_url(cls, v):
+        if not v.startswith(("http://", "https://")):
+            raise ValueError("webhook_url must be a valid HTTP or HTTPS URL")
+        return v
+
+    @field_validator("duration")
+    def check_duration_positive(cls, value):
+        if value <= 0:
+            raise ValueError("duration must be greater than 0")
+        return value
+
     model_config = {
         "json_schema_extra": {
             "examples": [
                 {
                     "duration": 30.5,
-                    "webhook_url": "https://webhook.site/94070b61-3ca5-4a17-8fc0-db7b8a8c1c8c"
+                    "webhook_url": "https://webhook.site/94070b61-3ca5-4a17-8fc0-db7b8a8c1c8c",
                 }
             ]
         }
